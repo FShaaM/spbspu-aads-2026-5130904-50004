c33e652773c82621b608b46159ec21bd8cf61d35
FShaaM <zinovevas135@gmail.com>
2026-06-18 16:37:22 +0300

    I edited tests 2.1

--- zinoviev.alexander/S2/test-all.cpp
+++ zinoviev.alexander/S2/test-all.cpp
@@ -1,13 +1,23 @@
-#define BOOST_TEST_MODULE StackTest
+#define BOOST_TEST_MODULE S2Tests
 
+#include <cstdlib>
 #include <boost/test/included/unit_test.hpp>
 #include "Stack.hpp"
+#include "evaluate.hpp"
 
 using namespace zinoviev;
 
+BOOST_AUTO_TEST_CASE(testLcm)
+{
+  BOOST_CHECK_EQUAL(lcm(2, 3), 6);
+  BOOST_CHECK_EQUAL(lcm(0, 5), 0);
+  BOOST_CHECK_EQUAL(lcm(-4, 6), 12);
+  BOOST_CHECK_EQUAL(lcm(12, 18), 36);
+}
+
 BOOST_AUTO_TEST_CASE(testStackPushTopPop)
 {
-  Stack<int> s;
+  Stack< int > s;
   BOOST_CHECK(s.empty());
   BOOST_CHECK_EQUAL(s.size(), 0);
 
@@ -31,22 +41,32 @@ BOOST_AUTO_TEST_CASE(testStackPushTopPop)
 
 BOOST_AUTO_TEST_CASE(testStackTopException)
 {
-  Stack<int> s;
+  Stack< int > s;
   BOOST_CHECK_THROW(s.top(), std::logic_error);
 }
 
 BOOST_AUTO_TEST_CASE(testStackCopyAndAssignment)
 {
-  Stack<int> s1;
+  Stack< int > s1;
   s1.push(1);
   s1.push(2);
 
-  Stack<int> s2(s1);
+  Stack< int > s2(s1);
   BOOST_CHECK_EQUAL(s2.top(), 2);
   s2.pop();
   BOOST_CHECK_EQUAL(s2.top(), 1);
 
-  Stack<int> s3;
+  Stack< int > s3;
   s3 = s1;
   BOOST_CHECK_EQUAL(s3.top(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(testStackWithStrings)
+{
+  Stack< std::string > s;
+  s.push("hello");
+  s.push("world");
+  BOOST_CHECK_EQUAL(s.top(), "world");
+  s.pop();
+  BOOST_CHECK_EQUAL(s.top(), "hello");
 }
