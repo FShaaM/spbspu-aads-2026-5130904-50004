70a518fb63bda25d786b387de0637cc663b567d0
FShaaM <zinovevas135@gmail.com>
2026-06-18 16:17:59 +0300

    I edited tests

--- zinoviev.alexander/S2/test-stack.cpp
+++ zinoviev.alexander/S2/test-stack.cpp
@@ -1,6 +1,5 @@
 #define BOOST_TEST_MODULE StackTest
 
-#include <stdexcept>
 #include <boost/test/included/unit_test.hpp>
 #include "Stack.hpp"
 
@@ -50,4 +49,24 @@ BOOST_AUTO_TEST_CASE(testStackCopyAndAssignment)
   Stack<int> s3;
   s3 = s1;
   BOOST_CHECK_EQUAL(s3.top(), 2);
+}
+
+BOOST_AUTO_TEST_CASE(testStackMove)
+{
+  Stack<int> s1;
+  s1.push(42);
+
+  Stack<int> s2(std::move(s1));
+  BOOST_CHECK_EQUAL(s2.top(), 42);
+  BOOST_CHECK(s1.empty());
+}
+
+BOOST_AUTO_TEST_CASE(testStackWithStrings)
+{
+  Stack<std::string> s;
+  s.push("hello");
+  s.push("world");
+  BOOST_CHECK_EQUAL(s.top(), "world");
+  s.pop();
+  BOOST_CHECK_EQUAL(s.top(), "hello");
 }
